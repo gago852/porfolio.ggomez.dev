2d7e2e5d33e6fae2981a0eeba3d14d2d2d909256
GABRIEL GOMEZ <ggomez.dev@outlook.com>
2025-03-05 04:42:37 +0000

    Add new images, update experience details, and enhance devcontainer settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import robotsTxt from "astro-robots-txt"
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
+// TODO: Add your site URL
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  site: 'https://example.com',
+});
